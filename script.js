186067116a740a8f1f9ca2261d284a713ebce3f8
Saief <a.s.bokor@outlook.com>
2024-10-01 02:27:25 -1100

    added some objects to the script.js file and then added them to an array labelled taskArr

---
+++ script.js
@@ -0,0 +1,34 @@
+// Define Objects
+const object1 = {
+    name: "General Graardor",
+    imagePath: "img/General Graardor.png",
+    message: "Kill 5 General Graardor",
+    bossLink: "link",
+    strategyLink: "Link"
+};
+
+const object2 = {
+    name: "General Zilyana",
+    imagePath: "img/General Graardor.png",
+    message: "Kill 5 General Zilyana",
+    bossLink: "link",
+    strategyLink: "Link"
+};
+
+const object3 = {
+    name: "General K'ril Tsutsaroth",
+    imagePath: "img/General Graardor.png",
+    message: "Kill 5 General K'ril Tsutsaroth",
+    bossLink: "link",
+    strategyLink: "Link"
+};
+
+const object4 = {
+    name: "General Kree'ara",
+    imagePath: "img/General Graardor.png",
+    message: "Kill 5 General Kree'ara",
+    bossLink: "link",
+    strategyLink: "Link"
+};
+
+const taskArr = [object1, object2, object3, object4];
