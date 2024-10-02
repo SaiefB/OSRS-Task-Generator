807fce082f5cfb016f99f186217f0619998f9a2a
Saief <a.s.bokor@outlook.com>
2024-10-02 17:19:55 +0100

    Added many more boss images and links to objects

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ const object6 = {
 const object7 = {
     name: "Giant Mole",
     imagePath: "img/Giant Mole.png",
-    message: "Kill 10 Giant Mole Dragon",
+    message: "Kill 10 Giant Mole",
     bossLink: "https://oldschool.runescape.wiki/w/Giant Mole",
     strategyLink: "https://oldschool.runescape.wiki/w/Giant Mole/Strategies"
 };
@@ -60,7 +60,7 @@ const object7 = {
 const object8 = {
     name: "Chaos Fanatic",
     imagePath: "img/Chaos Fanatic.png",
-    message: "Kill 10 Chaos Fanatic Dragon",
+    message: "Kill 10 Chaos Fanatic",
     bossLink: "https://oldschool.runescape.wiki/w/Chaos Fanatic",
     strategyLink: "https://oldschool.runescape.wiki/w/Chaos Fanatic/Strategies"
 };
@@ -68,14 +68,246 @@ const object8 = {
 const object9 = {
     name: "Dagannoth Kings",
     imagePath: "img/Dagannoth Rex.png",
-    message: "Kill 10 Dagannoth Kings Dragon",
+    message: "Kill 10 Dagannoth Kings",
     bossLink: "https://oldschool.runescape.wiki/w/Dagannoth Kings",
     strategyLink: "https://oldschool.runescape.wiki/w/Dagannoth Kings/Strategies"
 };
  
+const object10 = {
+    name: "Obor",
+    imagePath: "img/Obor.png",
+    message: "Kill 5 Obor",
+    bossLink: "https://oldschool.runescape.wiki/w/Obor",
+    strategyLink: "https://oldschool.runescape.wiki/w/Obor"
+};
+ 
+const object11 = {
+    name: "Obor",
+    imagePath: "img/Obor.png",
+    message: "Kill 5 Obor",
+    bossLink: "https://oldschool.runescape.wiki/w/Obor",
+    strategyLink: "https://oldschool.runescape.wiki/w/Obor"
+};
+ 
+const object12 = {
+    name: "Kalphite Queen",
+    imagePath: "img/Kalphite Queen.png",
+    message: "Kill 5 Kalphite Queen",
+    bossLink: "https://oldschool.runescape.wiki/w/Kalphite Queen",
+    strategyLink: "https://oldschool.runescape.wiki/w/Kalphite_Queen/Strategies"
+};
+ 
+const object13 = {
+    name: "Vorkath",
+    imagePath: "img/Vorkath.png",
+    message: "Kill 5 Vorkath",
+    bossLink: "https://oldschool.runescape.wiki/w/Vorkath",
+    strategyLink: "https://oldschool.runescape.wiki/w/Vorkath/Strategies"
+};
+ 
+const object14 = {
+    name: "Scurrius",
+    imagePath: "img/Scurrius.png",
+    message: "Kill 5 Scurrius",
+    bossLink: "https://oldschool.runescape.wiki/w/Scurrius",
+    strategyLink: "https://oldschool.runescape.wiki/w/Scurrius/Strategies"
+};
+ 
+const object15 = {
+    name: "Barrows",
+    imagePath: "img/Barrows.png",
+    message: "Kill 5 Barrows",
+    bossLink: "https://oldschool.runescape.wiki/w/Barrows",
+    strategyLink: "https://oldschool.runescape.wiki/w/Barrows/Strategies"
+};
+ 
+const object16 = {
+    name: "Artio",
+    imagePath: "img/Artio.png",
+    message: "Kill 5 Artio",
+    bossLink: "https://oldschool.runescape.wiki/w/Artio",
+    strategyLink: "https://oldschool.runescape.wiki/w/Artio/Strategies"
+};
+ 
+const object17 = {
+    name: "Artio",
+    imagePath: "img/Artio.png",
+    message: "Kill 5 Artio",
+    bossLink: "https://oldschool.runescape.wiki/w/Artio",
+    strategyLink: "https://oldschool.runescape.wiki/w/Artio/Strategies"
+};
+ 
+const object18 = {
+    name: "Bryophyta",
+    imagePath: "img/Bryophyta.png",
+    message: "Kill 5 Bryophyta",
+    bossLink: "https://oldschool.runescape.wiki/w/Bryophyta",
+    strategyLink: "https://oldschool.runescape.wiki/w/Bryophyta"
+};
+ 
+const object19 = {
+    name: "Calvar'ion",
+    imagePath: "img/Calvar'ion.png",
+    message: "Kill 5 Calvar'ion",
+    bossLink: "https://oldschool.runescape.wiki/w/Calvar%27ion",
+    strategyLink: "https://oldschool.runescape.wiki/w/Vet%27ion/Strategies"
+};
+ 
+const object20 = {
+    name: "Callisto",
+    imagePath: "img/Callisto.png",
+    message: "Kill 5 Callisto",
+    bossLink: "https://oldschool.runescape.wiki/w/Callisto",
+    strategyLink: "https://oldschool.runescape.wiki/w/Callisto/Strategies"
+};
+ 
+const object21 = {
+    name: "Chaos Elemental",
+    imagePath: "img/Chaos Elemental.png",
+    message: "Kill 5 Chaos Elemental",
+    bossLink: "https://oldschool.runescape.wiki/w/Chaos_Elemental",
+    strategyLink: "https://oldschool.runescape.wiki/w/Chaos_Elemental/Strategies"
+};
+ 
+const object22 = {
+    name: "Crazy Archaeologist",
+    imagePath: "img/Crazy archaeologist.png",
+    message: "Kill 5 Crazy Archaeologist",
+    bossLink: "https://oldschool.runescape.wiki/w/Crazy_archaeologist",
+    strategyLink: "https://oldschool.runescape.wiki/w/Crazy_archaeologist/Strategies"
+};
+ 
+const object23 = {
+    name: "Deranged Archaeologist",
+    imagePath: "img/Deranged archaeologist.png",
+    message: "Kill 5 Deranged archaeologist",
+    bossLink: "https://oldschool.runescape.wiki/w/Deranged_archaeologist",
+    strategyLink: "https://oldschool.runescape.wiki/w/Deranged_archaeologist/Strategies"
+};
+ 
+const object24 = {
+    name: "Grotesque Guardians",
+    imagePath: "img/Dawn.png",
+    message: "Kill 5 Grotesque Guardians",
+    bossLink: "https://oldschool.runescape.wiki/w/Grotesque_Guardians#The_fight",
+    strategyLink: "https://oldschool.runescape.wiki/w/Grotesque_Guardians/Strategies"
+};
+ 
+const object25 = {
+    name: "Hespori",
+    imagePath: "img/Hespori.png",
+    message: "Kill Hespori",
+    bossLink: "https://oldschool.runescape.wiki/w/Hespori",
+    strategyLink: "https://oldschool.runescape.wiki/w/Hespori"
+};
+ 
+const object26 = {
+    name: "The Mimic",
+    imagePath: "img/The Mimic.png",
+    message: "Kill The Mimic",
+    bossLink: "https://oldschool.runescape.wiki/w/The_Mimic",
+    strategyLink: "https://oldschool.runescape.wiki/w/The_Mimic/Strategies"
+};
+ 
+const object27 = {
+    name: "Moons of Peril",
+    imagePath: "img/Blood Moon.png",
+    message: "Take on the Moons of Peril 5 times",
+    bossLink: "https://oldschool.runescape.wiki/w/Moons_of_Peril",
+    strategyLink: "https://oldschool.runescape.wiki/w/Moons_of_Peril/Strategies"
+};
+ 
+const object28 = {
+    name: "Scorpia",
+    imagePath: "img/Scorpia.png",
+    message: "Kill 5 Scorpia",
+    bossLink: "https://oldschool.runescape.wiki/w/Scorpia",
+    strategyLink: "https://oldschool.runescape.wiki/w/Scorpia/Strategies"
+};
+ 
+const object29 = {
+    name: "Skotizo",
+    imagePath: "img/Skotizo.png",
+    message: "Kill Skotizo or Grab one Dark Totem piece",
+    bossLink: "https://oldschool.runescape.wiki/w/Skotizo",
+    strategyLink: "https://oldschool.runescape.wiki/w/Skotizo/Strategies"
+};
+ 
+const object30 = {
+    name: "Spindel",
+    imagePath: "img/Spindel.png",
+    message: "Kill 5 Spindel",
+    bossLink: "https://oldschool.runescape.wiki/w/Spindel",
+    strategyLink: "https://oldschool.runescape.wiki/w/Venenatis/Strategies"
+};
+ 
+const object31 = {
+    name: "Tempoross",
+    imagePath: "img/Tempoross.png",
+    message: "Kill Tempoross 10 times",
+    bossLink: "https://oldschool.runescape.wiki/w/Tempoross",
+    strategyLink: "https://oldschool.runescape.wiki/w/Tempoross/Strategies"
+};
+ 
+const object32 = {
+    name: "ToB",
+    imagePath: "img/Theatre of Blood logo.png",
+    message: "Complete Theatre of Blood",
+    bossLink: "https://oldschool.runescape.wiki/w/Theatre_of_Blood",
+    strategyLink: "https://oldschool.runescape.wiki/w/Theatre_of_Blood/Strategies"
+};
+ 
+const object33 = {
+    name: "ToA",
+    imagePath: "img/Tombs of Amascut.png",
+    message: "Complete Tombs of Amascut",
+    bossLink: "https://oldschool.runescape.wiki/w/Tombs_of_Amascut",
+    strategyLink: "https://oldschool.runescape.wiki/w/Tombs_of_Amascut/Strategies"
+};
+ 
+const object34 = {
+    name: "Jad",
+    imagePath: "img/TzTok-Jad.png",
+    message: "Kill Jad",
+    bossLink: "https://oldschool.runescape.wiki/w/TzTok-Jad",
+    strategyLink: "https://oldschool.runescape.wiki/w/TzHaar_Fight_Cave/Strategies#TzTok-Jad"
+};
+ 
+const object35 = {
+    name: "Venenatis",
+    imagePath: "img/Venenatis.png",
+    message: "Kill 5 Venenatis",
+    bossLink: "https://oldschool.runescape.wiki/w/Venenatis",
+    strategyLink: "https://oldschool.runescape.wiki/w/Venenatis/Strategies"
+};
+ 
+const object36 = {
+    name: "Vet'ion",
+    imagePath: "img/Vet'ion.png",
+    message: "Kill 5 Vet'ion",
+    bossLink: "https://oldschool.runescape.wiki/w/Vet%27ion",
+    strategyLink: "https://oldschool.runescape.wiki/w/Vet%27ion/Strategies"
+};
+ 
+const object37 = {
+    name: "Wintertodt",
+    imagePath: "img/WT.png",
+    message: "Complate 10 Wintertodt",
+    bossLink: "https://oldschool.runescape.wiki/w/Wintertodt",
+    strategyLink: "https://oldschool.runescape.wiki/w/Wintertodt/Strategies"
+};
+ 
+const object38 = {
+    name: "Zulrah",
+    imagePath: "img/z.png",
+    message: "Kill 5 Zulrah",
+    bossLink: "https://oldschool.runescape.wiki/w/Zulrah",
+    strategyLink: "https://oldschool.runescape.wiki/w/Zulrah/Strategies"
+};
+ 
 
 // Array of tasks
-const taskArr = [object1, object2, object3, object4, object5, object6, object7, object8, object9];
+const taskArr = [object1, object2, object3, object4, object5, object6, object7, object8, object9, object10, object11, object12, object13, object14, object15, object16, object17, object18, object19, object20, object21, object22, object23, object24, object25, object26, object27, object28, object29, object30, object31, object32, object33, object34, object35, object36, object37, object38];
 
 // Function to generate a random task
 function generateTask() {
